3df3438b8df5cb34af749072cea5320203012410
Justin Stone <justinstone@Justins-MBP-2.lan1>
2022-07-05 20:40:06 -0400

    Set up the app.gets and set up the port to listen at localhost

--- server.js
+++ server.js
@@ -4,5 +4,30 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const notes = require('./db/db.json');
+const { uuid } = require('./public/utils/id');
 
 const app = express();
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} get notes request recieved`);
+  res.json(notes);
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port http://localhost:${PORT}`)
+);
